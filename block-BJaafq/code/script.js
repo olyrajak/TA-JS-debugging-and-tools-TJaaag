d812a265ec1a368190b5901a2332c0e9e269e69e
Oly Rajak <59551153+olyrajak@users.noreply.github.com>
2022-09-21 01:05:30 +0530

    revision

--- block-BJaafq/code/script.js
+++ block-BJaafq/code/script.js
@@ -44,35 +44,35 @@ function multiply(numA, numB) {
 }
 
 // Addition calls
-test("add 10+5 to be 15", () => {
+test("add 10 and 5 to be 15", () => {
     expect(add(10, 5)).toEqual(15);
 });
-test("add 5+5 to be 10", () => {
+test("add 5 and 5 to be 10", () => {
     expect(add(5, 5)).toEqual(20);
 });
 test("add 30+9 to be 39", () => {
     expect(add(30, 9)).toEqual(39);
 });
-test("add 10+80 to be 90", () => {
+test("add 10 and 80 to be 90", () => {
     expect(add(10, 80)).toEqual(90);
 });
-test("add 25+5 to be 25", () => {
+test("add 25 and 5 to be 25", () => {
     expect(add(25, 5)).toEqual(20);
 });
 
 // Multiplication calls
-test("add 10*5 to be 50", () => {
+test("add 10 and 5 to be 50", () => {
     expect(multiply(10, 5)).toEqual(50);
 });
-test("add 5*5 to be 25", () => {
-    expect(multiply(5, 5)).toEqual(20);
+test("multiply 5 and 5 to be 25", () => {
+    expect(multiply(5, 5)).toEqual(25);
 });
-test("add 30*9 to be 270", () => {
+test("multiply 30 and 9 to be 270", () => {
     expect(multiply(30, 9)).toEqual(270);
 });
-test("add 10*80 to be 800", () => {
-    expect(multiply(10, 80)).toEqual(80);
+test("multiply 10 and 80 to be 800", () => {
+    expect(multiply(10, 80)).toEqual(800);
 });
-test("add 25*5 to be 125", () => {
-    expect(multiply(25, 5)).toEqual(120);
+test("multiply 25 and 5 to be 125", () => {
+    expect(multiply(25, 5)).toEqual(125);
 });
